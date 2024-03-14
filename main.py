8280e1cbc0c81cb0d5f953d821b3836719cc56c8
Jang-DongHo <donghoo18@naver.com>
2024-03-14 13:45:07 +0900

    [Project] 프로젝트 생성

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# test.py
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from time import sleep
-import os
-import sys
+from selenium_utils import get_driver
 
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)
+driver = get_driver()
 
 url = 'https://www.gnu.ac.kr/cse/na/ntt/selectNttList.do?mi=17093&bbsId=4753'
 
@@ -17,5 +12,6 @@ driver.implicitly_wait(5)
 sleep(3) 
 
 html = driver.page_source
+soup = BeautifulSoup(html, 'lxml')
 
-print(html)
+print(soup)
