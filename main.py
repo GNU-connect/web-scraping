76300e51d0e4e14bf2a313d05bf6639b82782eb8
Jang-DongHo <donghoo18@naver.com>
2024-03-22 13:46:00 +0900

    [Feat] 공지사항 스크래핑 코드 추가

--- main.py
+++ main.py
@@ -1,69 +1,28 @@
+import time
+from src.notice.scraper import Scraper
+from multiprocessing import Process
 from src.supabase_utils import supabase
-import requests
-from bs4 import BeautifulSoup
 
-# 전역 변수
-max_num_notices = 5
-college = 'inmun'
+def get_colleges():
+    colleges = supabase().table('college').select('college_en').execute().data
+    college_en_list = [college['college_en'] for college in colleges]
+    return college_en_list
 
-response = supabase().table(f'{college}-category').select("*").execute()
-datas = response.data
+def run_notice_scraper(college):
+    scraper = Scraper(college)
+    scraper.scrape_notice_data()
 
-# datas = [{
-#     'id': 40, 'category': '공지사항', 'mi': 10518, 'bbs_id': 1458, 'updated_at': '2024-03-15T08:39:15.494631+00:00', 'department': 'sophia', 'last_ntt_sn': 2229329}]
+if __name__ == '__main__':
+    start_time = time.time()
+    colleges = get_colleges()
 
-for data in datas:
-    category_id, department, mi, bbs_id, last_ntt_sn = data['id'], data['department'], data['mi'], data['bbs_id'], data['last_ntt_sn']
-    try:
-      notice_objects = []
-      base_url = f'https://www.gnu.ac.kr/{department}/na/ntt/'
-      department_board_url = f'{base_url}selectNttList.do?mi={mi}&bbsId={bbs_id}'
-
-      request = requests.get(department_board_url)
-      parsed_html = BeautifulSoup(request.text, 'html.parser')
-      
-      tbody_element = parsed_html.find('tbody')
-      new_notice_htmls = tbody_element.find_all('tr')
-
-      for new_notice_html in new_notice_htmls:
-          ntt_sn = new_notice_html.find('a', class_='nttInfoBtn')['data-id']
-          # ntt_sn가 last_ntt_sn보다 작거나 같으면 break
-          if int(ntt_sn) <= last_ntt_sn:
-              break
-          notice_id = new_notice_html.find('td', class_='BD_tm_none').text.strip()
-          title = new_notice_html.find('a').contents[0].strip()
-          notice_object = {
-              'department': department,
-              'category_id': category_id,
-              'title': title,
-              'ntt_sn': int(ntt_sn),
-          }
-          notice_objects.append(notice_object)
-      
-      # ntt_sn 기준으로 내림차순 뒤, 5개만 가져온다.
-      result = sorted(notice_objects, key=lambda x: x['ntt_sn'], reverse=True)[:5]
-
-      # 공지사항 카테고리의 last_ntt_sn을 업데이트한다.
-      #supabase().table(f'{college}-category').update({'last_ntt_sn': int(result[0]['ntt_sn'])}).eq('id', category_id).execute()
-
-      # 해당 학과 카테고리의 공지사항을 조회하여 개수를 확인한다.
-      existing_notices = supabase().from_(f'{college}-notice').select('id, ntt_sn').eq('category_id', category_id).execute().data
-      
-      # 만약 해당 학과 카테고리 테이블에 공지사항의 자리가 충분하다면, 새로운 공지사항을 삽입한다.
-      if len(result) <= max_num_notices - len(existing_notices):
-          supabase().table(f'{college}-notice').insert(result).execute()
-      # 충분하지 않다면, 오래된 공지사항의 자리부터 새로운 공지사항으로 교체한다.
-      else:
-          for newest_notice in result:
-            oldest_notice = min(existing_notices, key=lambda x: x['ntt_sn'])
-            oldest_notice_id = oldest_notice['id']
-            supabase().table(f'{college}-notice').update(newest_notice).eq('id', oldest_notice_id).execute()
-         
-    except Exception as e:
-      print(category_id, department, mi, bbs_id, e)
-      continue
+    processes = []
+    for college in colleges:
+        process = Process(target=run_notice_scraper, args=(college,))
+        processes.append(process)
+        process.start()
     
-
+    for process in processes:
+        process.join()
     
-
-    
+    print(f"모든 단과대학의 스크래핑이 완료되었습니다. 소요시간: {time.time() - start_time}초")
---
+++ src/notice/scraper.py
@@ -0,0 +1,97 @@
+from src.supabase_utils import supabase
+import requests
+from bs4 import BeautifulSoup
+import traceback
+
+max_num_notices = 5 # 스크래핑 할 공지사항 개수 (변경 X)
+
+class Scraper:
+    def __init__(self, college):
+        self.college = college
+
+    def scrape_notice_data(self):
+        # 학과별 카테고리 데이터 가져오기
+        datas = self.get_category_data(self.college)
+        if datas is None:
+          print(f'{self.college}의 카테고리 데이터를 가져오는데 실패했습니다.')
+          return
+        
+        # 학과 카테고리별 공지사항 스크래핑
+        for data in datas:
+          category_id = data['id']
+          department_id = data['department_id']
+          department_en = data['department']['department_en']
+          mi = data['mi']
+          bbs_id = data['bbs_id']
+          last_ntt_sn = data['last_ntt_sn']
+
+          notice_objects = []
+          base_url = f'https://www.gnu.ac.kr/{department_en}/na/ntt/'
+          department_board_url = f'{base_url}selectNttList.do?mi={mi}&bbsId={bbs_id}'
+
+          try:
+            request = requests.get(department_board_url)
+            parsed_html = BeautifulSoup(request.text, 'html.parser')
+
+            tbody_element = parsed_html.find('tbody')
+            new_notice_htmls = tbody_element.find_all('tr')
+
+            # 새로운 공지사항 확인
+            for new_notice_html in new_notice_htmls:
+                ntt_sn = new_notice_html.find('a', class_='nttInfoBtn')['data-id']
+                if int(ntt_sn) <= last_ntt_sn:
+                    break
+                title = new_notice_html.find('a').contents[0].strip()
+                notice_object = {
+                    'department_id': department_id,
+                    'category_id': category_id,
+                    'title': title,
+                    'ntt_sn': int(ntt_sn),
+                }
+                notice_objects.append(notice_object)
+            
+            if len(notice_objects) == 0:
+                continue
+            
+            # 최신 공지사항 5개만 가져오기
+            result = sorted(notice_objects, key=lambda x: x['ntt_sn'], reverse=True)[:5]
+
+            # 공지사항 삽입(또는 교체)
+            existing_notices = self.get_existing_notices(category_id)
+            if len(result) <= max_num_notices - len(existing_notices):
+                self.insert_notices(result)
+            else:
+                for newest_notice in result:
+                    oldest_notice = min(existing_notices, key=lambda x: x['ntt_sn'])
+                    oldest_notice_id = oldest_notice['id']
+                    self.update_notice(newest_notice, oldest_notice_id)
+            self.update_category_last_ntt_sn(category_id, result[0]['ntt_sn'])
+            print(f"{department_en}의 {category_id}번 카테고리의 새로운 공지사항 {len(result)}개를 스크래핑했습니다.")
+
+          except Exception as e:
+            tb = e.__traceback__
+            tb_info = traceback.extract_tb(tb)
+            for line in tb_info:
+                filename, line_no, func_name, source_code = line
+                print(f"{filename}:{line_no} 에서 {e.__class__.__name__} 발생")
+            print(f'스크래핑 실패: {department_en}의 {category_id}번 카테고리를 {e} 의 사유로 실패했습니다.')
+            continue
+    
+    def get_category_data(self, college):
+        try:
+          datas = supabase().table(f'{college}-category').select(f'*, department(department_en)').execute().data
+          return datas
+        except Exception as e:
+          return None
+
+    def update_category_last_ntt_sn(self, category_id, last_ntt_sn):
+        supabase().table(f'{self.college}-category').update({'last_ntt_sn': int(last_ntt_sn)}).eq('id', category_id).execute()
+
+    def get_existing_notices(self, category_id):
+        return supabase().from_(f'{self.college}-notice').select('id, ntt_sn').eq('category_id', category_id).execute().data
+
+    def insert_notices(self, notices):
+        supabase().table(f'{self.college}-notice').insert(notices).execute()
+
+    def update_notice(self, notice, notice_id):
+        supabase().table(f'{self.college}-notice').update(notice).eq('id', notice_id).execute()
