e98943194f96c0e9bbd1fb6afdc8a78bca9cf28e
Jang-DongHo <donghoo18@naver.com>
2024-04-23 19:44:50 +0900

    [Refactor] 스크래핑 속도 개선 (기존 60초 이상 -> 36초)

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import time
+from multiprocessing import Pool
 from src.notice.scraper import Notice_Scraper
 from src.cafeteria.scraper import Cafeteria_Scraper
 from src.academic_calendar.scraper import AcademicCalendarScraper
-from multiprocessing import Process
 from src.supabase_utils import supabase
 from webdriver_manager.chrome import ChromeDriverManager
+from datetime import datetime, timedelta
 
 # 셀레니움 드라이버 로드
 driver_path = ChromeDriverManager().install()
 
+def delete_oldest_dishes():
+    now_date = datetime.now() - timedelta(days=1)
+    supabase().table('cafeteria-diet').delete().lt('date', now_date).execute()
+
 def get_colleges():
-    colleges = supabase().table('college').select('college_en, etc_value').execute().data
-    college_en_list = [college['college_en'] for college in colleges if college['etc_value'] == False]
-    college_en_list.append('etc')
-    return college_en_list
+    return [college['college_en'] for college in supabase().table('college').select('college_en, etc_value').execute().data if college['etc_value'] == False] + ['etc']
+
+def get_cafeterias():
+    return supabase().table('cafeteria').select('*').execute().data
 
 def run_notice_scraper(college):
     notice_scraper = Notice_Scraper(college)
     notice_scraper.scrape_notice_data()
 
-def run_cafeteria_scraper():
-    cafeteria_scraper = Cafeteria_Scraper(driver_path)
-    cafeteria_scraper.delete_oldest_dishes()
+def run_cafeteria_scraper(cafeteria):
+    cafeteria_scraper = Cafeteria_Scraper(driver_path, cafeteria)
     cafeteria_scraper.scrape_cafeteria_dish_data()
 
 def run_academic_calendar_scraper():
@@ -30,24 +34,23 @@ def run_academic_calendar_scraper():
 
 if __name__ == '__main__':
     start_time = time.time()
+    cafeterias = get_cafeterias()
     colleges = get_colleges()
 
-    processes = []
-    # 단과대학별 공지사항 스크래핑
-    for college in colleges:
-        process = Process(target=run_notice_scraper, args=(college,))
-        processes.append(process)
-        process.start()
-    # 학사일정 데이터 스크래핑
-    process = Process(target=run_academic_calendar_scraper)
-    processes.append(process)
-    process.start()
-    # 학식 데이터 스크래핑
-    process = Process(target=run_cafeteria_scraper)
-    processes.append(process)
-    process.start()
+    # 공지사항 스크래핑 작업
+    with Pool() as pool:
+       pool.map(run_notice_scraper, colleges)
+
+    # 학사 일정 및 학식 스크래핑 작업
+    delete_oldest_dishes()
+    with Pool() as pool2:
+        pool2.map(run_cafeteria_scraper, cafeterias)
+        pool2.close()
+        pool2.join()
     
-    for process in processes:
-        process.join()
+    with Pool() as pool3:
+       pool3.apply_async(run_academic_calendar_scraper)
+       pool3.close()
+       pool3.join()
     
-    print(f"모든 웹페이지의 정보 스크래핑이 완료되었습니다. 소요시간: {time.time() - start_time}초")
+    print(f"모든 웹페이지의 정보 스크래핑이 완료되었습니다. 소요시간: {time.time() - start_time:.2f}초")
--- src/cafeteria/scraper.py
+++ src/cafeteria/scraper.py
@@ -2,15 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import traceback
-from datetime import datetime, timedelta
+from datetime import datetime
 from src.supabase_utils import supabase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 class Cafeteria_Scraper:
-    def __init__(self, driver_path):
+    def __init__(self, driver_path, cafeteria):
         self.driver = None
         self.driver_path = driver_path
+        self.cafeteria = cafeteria
     
     def __enter__(self):
         options = Options()
@@ -24,153 +25,138 @@ class Cafeteria_Scraper:
             self.driver.quit()
     
     def scrape_cafeteria_dish_data(self):
-        datas = self.get_cafeteria_data()
-        if datas is None:
-          print('식단 데이터 조회 실패: 식단 데이터를 가져오는데 실패했습니다.')
-          return
-        # 식단 데이터 스크래핑
-        for data in datas:
-          campus_id = data['campus_id']
-          cafeteria_id = data['id']
-          cafeteria_name_ko = data['cafeteria_name_ko']
-          mi = data['mi']
-          rest_seq = data['rest_seq']
-          type = data['type']
-          sch_sys_id = data['sch_sys_id']
-          form_type = data['form_type']
-          last_date = data['last_date']
-          last_date = datetime.fromisoformat(last_date)
+        data = self.cafeteria
+        
+        campus_id = data['campus_id']
+        cafeteria_id = data['id']
+        cafeteria_name_ko = data['cafeteria_name_ko']
+        mi = data['mi']
+        rest_seq = data['rest_seq']
+        type = data['type']
+        sch_sys_id = data['sch_sys_id']
+        form_type = data['form_type']
+        last_date = data['last_date']
+        last_date = datetime.fromisoformat(last_date)
 
-          base_url = f'https://www.gnu.ac.kr/{type}/ad/fm/foodmenu/selectFoodMenuView.do?restSeq={rest_seq}&mi={mi}'
-          if sch_sys_id != None:
-            base_url += f'&schSysId={sch_sys_id}'
+        base_url = f'https://www.gnu.ac.kr/{type}/ad/fm/foodmenu/selectFoodMenuView.do?restSeq={rest_seq}&mi={mi}'
+        if sch_sys_id != None:
+          base_url += f'&schSysId={sch_sys_id}'
 
-          try:
-            with self as scraper:
-              scraper.driver.get(base_url)
-              scraper.driver.implicitly_wait(10)
-              # 이번 주와 다음 주의 식단 데이터를 가져옴
-              for i in range(2):
-                parsed_html = scraper.driver.page_source
-                
-                parsed_html = BeautifulSoup(parsed_html, 'html.parser')
-                
-                thead_element = parsed_html.find('thead')
-                tbody_element = parsed_html.find('tbody')
-
-                dish_headers = thead_element.find_all('th')[1:]
-
-                date_list = [datetime.strptime(dish_header.text.strip().split(' ')[1], '%Y-%m-%d') for dish_header in dish_headers]
-                dish_type_list = ['주식', '국류', '찬류', '후식']
-                day_list = ['월', '화', '수', '목', '금', '토', '일']
-                time_list = ['아침', '점심', '저녁']
-                
-                dish_time_htmls = tbody_element.find_all('tr')
-
-                result = []
-                # 행(시간) 단위로 식단 데이터 처리
-                for t in range(len(dish_time_htmls) if len(dish_time_htmls) < len(time_list) else len(time_list)):
-                  dish_type = dish_type = dish_type_list[t] if form_type == 2 else None
-                  dish_time_html = dish_time_htmls[t]
-                  # 점심 메뉴만 제공해주는 식당일 경우 (ex. 교직원 식당)
-                  time = '점심' if cafeteria_name_ko == '교육문화1층식당' or form_type == 2 else time_list[t]
-                  dish_day_htmls = dish_time_html.find_all('td')
-                  # 열(요일) 단위로 식단 데이터 처리
-                  for d in range(len(day_list)):
-                      dish_object = {}
-                      dish_day_html = dish_day_htmls[d]
-                      date = date_list[d]
-                      if date.replace(tzinfo=last_date.tzinfo) <= last_date:
-                        continue
-                      day = day_list[d]
-                      dish_category = None
-                      categories = dish_day_html.find_all('div')
-                      for category in categories:
-                        # 카테고리 헤더 처리
-                        category_header = category.find('p', class_='mgt15')
-                        if category_header is not None:
-                          dish_category = category_header.text.strip()
-                        # 메뉴 처리
-                        dishes = category.find('p', class_='')
-                        if dishes is not None:
-                          dishes = dishes.get_text(separator='<br>').split('<br>')
-                          is_set_menu = False  # 변수를 설정하여 (세트메뉴)가 발견되었는지 추적합니다.
-                          tmp_dish = None # tmp 변수
-                          for dish_idx in range(len(dishes)):
-                            dish = dishes[dish_idx]
-                            if dish == ' ' or dish == '':
-                              continue
-                            # 중앙식당 점심과 저녁은 임의로 dish_category을 붙여줌
-                            if cafeteria_name_ko == '중앙1식당' and (time == '점심' or time == '저녁'):
-                              if dish == '(세트메뉴)':
-                                is_set_menu = True
-                                continue
-                              if not is_set_menu:
-                                dish_categorys = ['뚝빼기/비빔밥', '양식']
-                                dish_category = dish_categorys[dish_idx]
-                              else:
-                                dish_category = '세트메뉴'
-                            # 칠암 제2분관 식당의 경우, 메뉴가 '/'로 나뉘어져 있음
-                            elif cafeteria_name_ko == '칠암제2분관식당' and time == '아침':
-                              if dish.startswith('★'):
-                                dish_category = dish[1:]
-                                continue
-                              elif '(' in dish:
-                                tmp_dish = dish[1:-1]
-                                continue
-                              elif dish.startswith('-'):
-                                if dish_category.startswith('샐러드'):
-                                  dish = (dish.lstrip('-') + " " + tmp_dish).strip()
-                                else:
-                                  dish = tmp_dish
-                            elif cafeteria_name_ko == '학생식당' and time == '아침' and campus_id == 2:
-                              if "천원의 아침밥 사업 시행에 따라" in dish:
-                                break
-                              elif dish == "(천원의 아침밥)":
-                                dish_category = "천원의아침밥"
-                                continue
-                            elif cafeteria_name_ko == '교육문화1층식당':
-                              if dish.startswith('('):
-                                continue
-                            
-                            # TODO: 식단 데이터 저장
-                            dish_object = {
-                              'cafeteria_id': cafeteria_id,
-                              'date': date.isoformat(),
-                              'day': day,
-                              'dish_type': dish_type,
-                              'dish_category': dish_category,
-                              'time': time,
-                              'dish_name': dish
-                            }
-                            result.append(dish_object)
-                # 다음 주로 넘어가기 위해 다음 주 버튼 클릭
-                if i == 0:
-                  scraper.driver.find_element(By.CLASS_NAME, "xi-angle-right").click()
-                  scraper.driver.implicitly_wait(10)
-              if len(result) > 0:
-                # 식단 데이터 삽입
-                self.insert_dishes(result)
-                # last_date 업데이트
-                self.update_cafeteria_last_date(cafeteria_id, result[-1]['date'])
-                print(f'[교내 식당] {campus_id}번 캠퍼스 {cafeteria_name_ko}의 새로운 식단 데이터 {len(result)}개를 스크래핑했습니다.')
-                
-          except Exception as e:
-            print(f'[교내 식당] 교내 식당 데이터 조회 실패: {campus_id}번 캠퍼스의 {cafeteria_name_ko} 식단 데이터를 가져오는데 실패했습니다.')
-            print(f'[교내 식당] 해당 교내 식당 URL: {base_url}')
-            traceback.print_exc()
-            return
-    
-    def get_cafeteria_data(self):
         try:
-          datas = supabase().table('cafeteria').select('*').execute().data
-          return datas
+          with self as scraper:
+            scraper.driver.get(base_url)
+            scraper.driver.implicitly_wait(10)
+            # 이번 주와 다음 주의 식단 데이터를 가져옴
+            #for i in range(2):
+            parsed_html = scraper.driver.page_source
+            
+            parsed_html = BeautifulSoup(parsed_html, 'html.parser')
+            
+            thead_element = parsed_html.find('thead')
+            tbody_element = parsed_html.find('tbody')
+
+            dish_headers = thead_element.find_all('th')[1:]
+
+            date_list = [datetime.strptime(dish_header.text.strip().split(' ')[1], '%Y-%m-%d') for dish_header in dish_headers]
+            dish_type_list = ['주식', '국류', '찬류', '후식']
+            day_list = ['월', '화', '수', '목', '금', '토', '일']
+            time_list = ['아침', '점심', '저녁']
+            
+            dish_time_htmls = tbody_element.find_all('tr')
+
+            result = []
+            # 행(시간) 단위로 식단 데이터 처리
+            for t in range(len(dish_time_htmls) if len(dish_time_htmls) < len(time_list) else len(time_list)):
+              dish_type = dish_type = dish_type_list[t] if form_type == 2 else None
+              dish_time_html = dish_time_htmls[t]
+              # 점심 메뉴만 제공해주는 식당일 경우 (ex. 교직원 식당)
+              time = '점심' if cafeteria_name_ko == '교육문화1층식당' or form_type == 2 else time_list[t]
+              dish_day_htmls = dish_time_html.find_all('td')
+              # 열(요일) 단위로 식단 데이터 처리
+              for d in range(len(day_list)):
+                  dish_object = {}
+                  dish_day_html = dish_day_htmls[d]
+                  date = date_list[d]
+                  if date.replace(tzinfo=last_date.tzinfo) <= last_date:
+                    continue
+                  day = day_list[d]
+                  dish_category = None
+                  categories = dish_day_html.find_all('div')
+                  for category in categories:
+                    # 카테고리 헤더 처리
+                    category_header = category.find('p', class_='mgt15')
+                    if category_header is not None:
+                      dish_category = category_header.text.strip()
+                    # 메뉴 처리
+                    dishes = category.find('p', class_='')
+                    if dishes is not None:
+                      dishes = dishes.get_text(separator='<br>').split('<br>')
+                      is_set_menu = False  # 변수를 설정하여 (세트메뉴)가 발견되었는지 추적합니다.
+                      tmp_dish = None # tmp 변수
+                      for dish_idx in range(len(dishes)):
+                        dish = dishes[dish_idx]
+                        if dish == ' ' or dish == '':
+                          continue
+                        # 중앙식당 점심과 저녁은 임의로 dish_category을 붙여줌
+                        if cafeteria_name_ko == '중앙1식당' and (time == '점심' or time == '저녁'):
+                          if dish == '(세트메뉴)':
+                            is_set_menu = True
+                            continue
+                          if not is_set_menu:
+                            dish_categorys = ['뚝빼기/비빔밥', '양식']
+                            dish_category = dish_categorys[dish_idx]
+                          else:
+                            dish_category = '세트메뉴'
+                        # 칠암 제2분관 식당의 경우, 메뉴가 '/'로 나뉘어져 있음
+                        elif cafeteria_name_ko == '칠암제2분관식당' and time == '아침':
+                          if dish.startswith('★'):
+                            dish_category = dish[1:]
+                            continue
+                          elif '(' in dish:
+                            tmp_dish = dish[1:-1]
+                            continue
+                          elif dish.startswith('-'):
+                            if dish_category.startswith('샐러드'):
+                              dish = (dish.lstrip('-') + " " + tmp_dish).strip()
+                            else:
+                              dish = tmp_dish
+                        elif cafeteria_name_ko == '학생식당' and time == '아침' and campus_id == 2:
+                          if "천원의 아침밥 사업 시행에 따라" in dish:
+                            break
+                          elif dish == "(천원의 아침밥)":
+                            dish_category = "천원의아침밥"
+                            continue
+                        elif cafeteria_name_ko == '교육문화1층식당':
+                          if dish.startswith('('):
+                            continue
+                        
+                        # TODO: 식단 데이터 저장
+                        dish_object = {
+                          'cafeteria_id': cafeteria_id,
+                          'date': date.isoformat(),
+                          'day': day,
+                          'dish_type': dish_type,
+                          'dish_category': dish_category,
+                          'time': time,
+                          'dish_name': dish
+                        }
+                        result.append(dish_object)
+              # 다음 주로 넘어가기 위해 다음 주 버튼 클릭
+              #if i == 0:
+              #  scraper.driver.find_element(By.CLASS_NAME, "xi-angle-right").click()
+              #  scraper.driver.implicitly_wait(10)
+            if len(result) > 0:
+              # 식단 데이터 삽입
+              self.insert_dishes(result)
+              # last_date 업데이트
+              self.update_cafeteria_last_date(cafeteria_id, result[-1]['date'])
+              print(f'[교내 식당] {campus_id}번 캠퍼스 {cafeteria_name_ko}의 새로운 식단 데이터 {len(result)}개를 스크래핑했습니다.')
+              
         except Exception as e:
-          return None
-    
-    def delete_oldest_dishes(self):
-      now_date = datetime.now() - timedelta(days=1)
-      supabase().table('cafeteria-diet').delete().lt('date', now_date).execute()
+          print(f'[교내 식당] 교내 식당 데이터 조회 실패: {campus_id}번 캠퍼스의 {cafeteria_name_ko} 식단 데이터를 가져오는데 실패했습니다.')
+          print(f'[교내 식당] 해당 교내 식당 URL: {base_url}')
+          traceback.print_exc()
+          return
     
     def insert_dishes(self, dishes):
       supabase().table('cafeteria-diet').insert(dishes).execute()
