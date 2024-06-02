7d8e7d7fc462a3b3b664224d32873d0041b88746
Jang-DongHo <donghoo18@naver.com>
2024-06-02 14:02:32 +0900

    [Fix] 내년 학사일정도 확인하도록 로직 수정

--- src/academic_calendar/scraper.py
+++ src/academic_calendar/scraper.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 from bs4 import BeautifulSoup
 import traceback
 from datetime import datetime, timedelta
@@ -23,45 +25,50 @@ class AcademicCalendarScraper:
     def __exit__(self, exc_type, exc_value, traceback):
         if self.driver:
             self.driver.quit()
-
+    
     def scrape_academic_calendar_data(self):
         try:
             with self as scraper:
                 scraper.driver.get(self.base_url)
                 scraper.driver.implicitly_wait(10)
-                parsed_html = BeautifulSoup(scraper.driver.page_source, 'html.parser')
-
-                tbody_element = parsed_html.find('tbody')
-                a_elements = tbody_element.find_all('a')
-
                 result = []
-                for a_element in a_elements:
-                    # href 속성에서 날짜 정보를 추출
-                    onclick_value = a_element['href']
-                    start_date = datetime.strptime(onclick_value.split("'")[3], '%Y/%m/%d')
-                    end_date = datetime.strptime(onclick_value.split("'")[5], '%Y/%m/%d')
+                # 올해, 내년 학사일정 데이터를 가져오기 위해 2번 반복
+                for _ in range(2):
+                    parsed_html = BeautifulSoup(scraper.driver.page_source, 'html.parser')
 
-                    if end_date < datetime.now() - timedelta(days=1):
-                        continue
+                    tbody_element = parsed_html.find('tbody')
+                    a_elements = tbody_element.find_all('a')
 
-                    # 텍스트에서 '2학기 동계방학' 추출
-                    contents = a_element.get_text()
-                    # 카테고리 추출
-                    category_idx = contents.find('-')
-                    category = contents[1:category_idx]
-                    content_idx = contents.find(']')
-                    if content_idx != -1:
-                        content = contents[content_idx + 1:].strip()
-                    else:
-                        continue
+                    for a_element in a_elements:
+                        # href 속성에서 날짜 정보를 추출
+                        onclick_value = a_element['href']
+                        start_date = datetime.strptime(onclick_value.split("'")[3], '%Y/%m/%d')
+                        end_date = datetime.strptime(onclick_value.split("'")[5], '%Y/%m/%d')
 
-                    schedule_object = {
-                        'calendar_type': 1 if category == '학부' else 2,
-                        'start_date': start_date.isoformat(),
-                        'end_date': end_date.isoformat(),
-                        'content': content
-                    }
-                    result.append(schedule_object)
+                        if end_date < datetime.now() - timedelta(days=1):
+                            continue
+
+                        # 텍스트에서 '2학기 동계방학' 추출
+                        contents = a_element.get_text()
+                        # 카테고리 추출
+                        category_idx = contents.find('-')
+                        category = contents[1:category_idx]
+                        content_idx = contents.find(']')
+                        if content_idx != -1:
+                            content = contents[content_idx + 1:].strip()
+                        else:
+                            continue
+
+                        schedule_object = {
+                            'calendar_type': 1 if category == '학부' else 2,
+                            'start_date': start_date.isoformat(),
+                            'end_date': end_date.isoformat(),
+                            'content': content
+                        }
+                        result.append(schedule_object)
+                    next_year_button = self.driver.find_element(By.XPATH, '//*[@id="listForm"]/div/div[1]/div[1]/a[3]/i')
+                    next_year_button.click()
+                    time.sleep(1)
 
                 self.delete_schedules()
                 self.insert_schedules(result)
